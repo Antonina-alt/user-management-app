cb43170ee60db38638911800bfc2b78c5e6ec785
Antonia <antoniadashkevich@gmail.com>
2026-06-21 18:57:39 +0300

    Preparing for deployment on a single web service

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+
 const corsOptions = require('./config/cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const requireActiveUser = require('./middleware/requireActiveUser');
 const errorHandler = require('./middleware/errorHandler');
-const { port } = require('./config/env');
+const { port, nodeEnv } = require('./config/env');
 
 const app = express();
 
@@ -17,6 +19,16 @@ app.use(cookieParser());
 app.use('/api', requireActiveUser);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+
+if (nodeEnv === 'production') {
+  const clientDistPath = path.join(__dirname, '..', 'client', 'dist');
+
+  app.use(express.static(clientDistPath));
+
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.join(clientDistPath, 'index.html'));
+  });
+}
 
 app.use(errorHandler);
 
